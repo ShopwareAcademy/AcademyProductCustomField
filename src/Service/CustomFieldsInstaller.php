adaafd5322fd37ac653b5133b3f6e31096d45ba7
Micha <m.hobert@shopware.com>
2024-12-09 16:47:39 +0100

    first commit

---
+++ src/Service/CustomFieldsInstaller.php
@@ -0,0 +1,75 @@
+<?php declare(strict_types=1);
+
+namespace Academy\Service;
+
+use Shopware\Core\Defaults;
+use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
+use Shopware\Core\System\CustomField\CustomFieldTypes;
+
+class CustomFieldsInstaller
+{
+    private const CUSTOM_FIELDSET_NAME = 'academy_product_set';
+
+    private const CUSTOM_FIELDSET = [
+        'name' => self::CUSTOM_FIELDSET_NAME,
+        'config' => [
+            'label' => [
+                'en-GB' => 'Academy product custom field set',
+                'de-DE' => 'Akademie Produkt custom field set',
+                Defaults::LANGUAGE_SYSTEM => 'Academy custom field set'
+            ]
+        ],
+        'customFields' => [
+            [
+                'name' => 'academy_product_cargo',
+                'type' => CustomFieldTypes::BOOL,
+                'config' => [
+                    'label' => [
+                        'en-GB' => 'Cargo flag',
+                        'de-DE' => 'Fracht flag',
+                        Defaults::LANGUAGE_SYSTEM => 'Cargo flag'
+                    ],
+                    'customFieldPosition' => 1
+                ]
+            ]
+        ]
+    ];
+
+    public function __construct(
+        private readonly EntityRepository $customFieldSetRepository,
+        private readonly EntityRepository $customFieldSetRelationRepository
+    ) {
+    }
+
+    public function install(Context $context): void
+    {
+        $this->customFieldSetRepository->upsert([
+            self::CUSTOM_FIELDSET
+        ], $context);
+    }
+
+    public function addRelations(Context $context): void
+    {
+        $this->customFieldSetRelationRepository->upsert(array_map(function (string $customFieldSetId) {
+            return [
+                'customFieldSetId' => $customFieldSetId,
+                'entityName' => 'product',
+            ];
+        }, $this->getCustomFieldSetIds($context)), $context);
+    }
+
+    /**
+     * @return string[]
+     */
+    private function getCustomFieldSetIds(Context $context): array
+    {
+        $criteria = new Criteria();
+
+        $criteria->addFilter(new EqualsFilter('name', self::CUSTOM_FIELDSET_NAME));
+
+        return $this->customFieldSetRepository->searchIds($criteria, $context)->getIds();
+    }
+}
